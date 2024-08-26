a33ae9f032ade74bc112d2cb123702f4935cbc97
Andrew Orta <stevenorta7@gmail.com>
2024-08-26 09:08:41 -0700

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,21 @@
-console.log('Hello World!')
+console.log("hello from inside the main.js file")
+
+//let myVar;
+
+const myVar = "andrew";
+const myVarType = typeof(myVar)
+console.log("myVarType " + myVarType);
+console.log(`myVarType ${myVarType}`);
+
+
+
+function runNow ()  {
+    if(myVarType === "number") {
+        console.log(`will 11 run?`);
+    } else {
+        console.log(`will 13 run?`);
+    }
+}
+
+runNow();
+runNow();
