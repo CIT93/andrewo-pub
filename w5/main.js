cfa7b78ebf8d1b3866a81b8d7bfce73c10699875
Andrew Orta <stevenorta7@gmail.com>
2024-09-12 16:43:46 -0700

    setup and array of objects

--- w5/main.js
+++ w5/main.js
@@ -34,57 +34,93 @@ function determineHouseHoldPts(numberInHousehold) {
   return houseHoldPoints;
 }
 
+function displayOutObj(obj) {
+  console.log(obj);
+  const output = document.getElementById("output");
+  const newH2 = document.createElement("h2");
+  newH2.textContent = `Carbon Footprint ${obj.cfpTotal}`;
+  const newH3 = document.createElement("h3");
+  newH3.textContent = `Based on number in and size of home`
+  const newP = document.createElement("p");
+  newP.textContent = `This number is based on the number of people in the house of ${obj.houseM} (score: ${obj.houseMPTS}),`;
+  newP.textContent += ` and a ${obj.houseS} size of home (score:${obj.houseSPTS}).`;
+  output.appendChild(newH2);
+  output.appendChild(newH3);
+  output.appendChild(newP);
+}
+
+
 function start(houseHoldMembers, houseSize) {
   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
   const houseSizePts = determineHouseSizePts(houseSize);
   const total = houseHoldPTS + houseSizePts;
-  cfpData.push([houseHoldMembers, houseSize, houseHoldPTS, houseSizePts, total]);
+  // const cfpObj = {
+  //   houseM: houseHoldMembers,
+  //   houseS: houseSize, 
+  //   houseMPTS: houseHoldPTS,
+  //   houseSPTS: houseSizePts,
+  //   cfpTotal: total
+  //   }
+  cfpData.push({
+    houseM: houseHoldMembers,
+    houseS: houseSize, 
+    houseMPTS: houseHoldPTS,
+    houseSPTS: houseSizePts,
+    cfpTotal: total
+    });
+
+    // displayOutObj(cfpObj)
+  
+  // cfpData.push([
+  //   houseHoldMembers, 
+  //   houseSize, 
+  //   houseHoldPTS, 
+  //   houseSizePts, 
+  //   total
+  // ]);
 }
 
 function displayOutput() {
-  for (arr of cfpData) {
-    const output = document.getElementById("output");
-    const newH2 = document.createElement("h2");
-    newH2.textContent = `Carbon Footprint ${arr[4]}`;
-    const newH3 = document.createElement("h3");
-    newH3.textContent = `Based on number in and size of home`
-    const newP = document.createElement("p");
-    newP.textContent = `This number is based on the number of people in the house of ${arr[0]} (score: ${arr[3]}),`;
-    newP.textContent += ` and a ${arr[1]} size of home (score:${arr[2]}).`;
-    output.appendChild(newH2);
-    output.appendChild(newH3);
-    output.appendChild(newP);
-  }
+ for (obj of cfpData) {
+  console.log(obj)
+   const output = document.getElementById("output");
+   const newH2 = document.createElement("h2");
+   newH2.textContent = `Carbon Footprint ${obj.cfpTotal}`;
+  //  const newH3 = document.createElement("h3");
+  //  newH3.textContent = `Based on number in and size of home`
+  //  const newP = document.createElement("p");
+  //  newP.textContent = `This number is based on the number of people in the house of ${arr[0]} (score: ${arr[3]}),`;
+  //  newP.textContent += ` and a ${arr[1]} size of home (score:${arr[2]}).`;
+   output.appendChild(newH2);
+  //  output.appendChild(newH3);
+  //  output.appendChild(newP);
+ }
 }
 
 
-start(1, "apt");
-start(2, "apt");
-start(3, "apt");
-start(4, "apt");
-start(5, "apt");
-start(6, "apt");
-start(7, "apt");
-start(1, "small");
-start(2, "small");
-start(3, "small");
-start(4, "small");
-start(5, "small");
-start(6, "small");
-start(7, "small");
-start(1, "medium");
-start(2, "medium");
-start(3, "medium");
-start(4, "medium");
-start(5, "medium");
-start(6, "medium");
-start(7, "medium");
-start(1, "large");
-start(2, "large");
-start(3, "large");
-start(4, "large");
-start(5, "large");
-start(6, "large");
-start(7, "large");
+// function displayOutput() {
+//  for (let i = 0; i < cfpData.length; i++) {
+//   console.log(i)
+//    const output = document.getElementById("output");
+//    const newH2 = document.createElement("h2");
+//    newH2.textContent = `Carbon Footprint ${cfpData[i][4]}`;
+//    const newH3 = document.createElement("h3");
+//    newH3.textContent = `Based on number in and size of home`
+//    const newP = document.createElement("p");
+//    newP.textContent = `This number is based on the number of people in the house of ${cfpData[i][0]} (score: ${cfpData[i][3]}),`;
+//    newP.textContent += ` and a ${cfpData[i][1]} size of home (score:${cfpData[i][2]}).`;
+//    output.appendChild(newH2);
+//    output.appendChild(newH3);
+//    output.appendChild(newP);
+//  }
+// }
 
-displayOutput();
+start(2, "apt");
+start(10, "large");
+start(2, "small");
+start(4, "medium");
+
+displayOutput()
+
+
+
