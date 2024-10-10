2180774ef0d564e597a12cc46a296d6fac6f024c
Andrew Orta <stevenorta7@gmail.com>
2024-10-09 19:07:08 -0700

    fixed validation/code from watching solution

--- w9/main.js
+++ w9/main.js
@@ -3,6 +3,10 @@ import {determineHouseHoldPts, determineHouseSizePts} from "./cfp.js";
 import {FORM} from "./global.js";
 import {saveLS, cfpData} from "./storage.js";
 
+const firstNameEl = document.getElementById('firstName');
+const lastNameEl = document.getElementById('lastName');
+const submitEl = document.getElementById("submitError");
+
 function start(first, last, houseHoldMembers, houseSize) {
   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
   const houseSizePts = determineHouseSizePts(houseSize);
@@ -19,29 +23,42 @@ function start(first, last, houseHoldMembers, houseSize) {
 }
 
 renderTbl(cfpData);
-  
+// Function to validate a single field
+function validateField (event) {
+  const field = event.target.value;
+  const fieldId = event.target.id;
+  const fieldError = document.getElementById(`${fieldId}Error`);
+
+  if (field === '') {
+    fieldError.textContent = `${fieldId} is required`;
+    event.target.classList.add('invalid');
+  } else {
+    fieldError.textContent = '';
+    event.target.classList.remove('invalid');
+  }
+}
+
+// Attach blur event listeners
+firstNameEl.addEventListener('blur', validateField);
+lastNameEl.addEventListener('blur', validateField);
+
 
 FORM.addEventListener('submit', function(e) {
   e.preventDefault();
   const firstName = FORM.firstname.value;
   const lastName = FORM.lastname.value;
-  const errorElement = document.getElementById('error');
-  let messages = []
-  if (firstName.value === '' || firstName.value == null) {
-    //messages.push('Name is required')
-  }
-  if (firstName.value === '' || firstName.value == null) {
-    //messages.push('Name is required')
-  }
-  if (messages.length > 0) {
-    e.preventDefault()
-    errorElement.textContent = messages.join(', ');
-  }
-  const houseMembers = parseInt(FORM.housem.value);
-  const houseSize = FORM.houses.value;
-  start(firstName, lastName, houseMembers, houseSize);
-  saveLS(cfpData);
-  renderTbl(cfpData);
-  FORM.reset();
+  const firstNameIsValid = firstNameEl.value !== '';
+  const lastNameIsValid = lastNameEl.value !== '';
+  if (firstNameIsValid && lastNameIsValid) {
+    submitEl.textContent = '';
+    const houseMembers = parseInt(FORM.housem.value);
+    const houseSize = FORM.houses.value;
+    start(firstName, lastName, houseMembers, houseSize);
+    saveLS(cfpData);
+    renderTbl(cfpData);
+    FORM.reset();
+  } else {
+    submitEl.textContent = "Form requires first name and last name";
+   }
 })
 
