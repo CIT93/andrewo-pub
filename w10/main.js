6be1b6c1d37a5f3c3d9ae1404a4a370344913ec4
Andrew Orta <stevenorta7@gmail.com>
2024-10-16 17:47:43 -0700

    w10 my attempt at the code, before I review other students code

--- w10/main.js
+++ w10/main.js
@@ -3,7 +3,7 @@ import {determineHouseHoldPts, determineHouseSizePts} from "./cfp.js";
 import {FORM, FNAME, LNAME, SUBMIT} from "./global.js";
 import {saveLS, cfpData} from "./storage.js";
 
-const start = function(first, last, houseHoldMembers, houseSize) {
+const start = (first, last, houseHoldMembers, houseSize) => {
   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
   const houseSizePts = determineHouseSizePts(houseSize);
   const total = houseHoldPTS + houseSizePts;
@@ -21,7 +21,7 @@ const start = function(first, last, houseHoldMembers, houseSize) {
 renderTbl(cfpData);
 
 // Function to validate a single field
-const validateField = function(event) {
+const validateField = event => {
   const field = event.target.value;
   const fieldId = event.target.id;
   const fieldError = document.getElementById(`${fieldId}Error`);
@@ -40,7 +40,7 @@ FNAME.addEventListener('blur', validateField);
 LNAME.addEventListener('blur', validateField);
 
 
-FORM.addEventListener('submit', function(e) {
+FORM.addEventListener('submit', e => {
   e.preventDefault();
   if (FNAME.value !== '' && LNAME.value !== '') {
     SUBMIT.textContent = '';
@@ -53,14 +53,19 @@ FORM.addEventListener('submit', function(e) {
    }
 });
 
+// rest operator
+// const add2 = function(...a) {
+//   return 2 + a[3];
+// }
 
-const add2 = function(...a) {
-  return 2 + a[3];
-}
+// const result = add2(1, 2, 3, 4);
 
-const result = add2(1, 2, 3, 4);
+// arrow function
 
-//spread argument
+const add2 = a => 2 + a;
+
+
+const result = add2(100);
 
 //IIFE
 
